686565f2b5693a51eb6c7369b9accea0c46f7cfc
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2025-01-26 00:58:10 +0100

    feat: Typescript for backend + PostgreSQL connection with Node.js

--- backend/index.ts
+++ backend/index.ts
@@ -1,28 +1,38 @@
+import 'dotenv/config';
+
 import cors from 'cors';
 import express from 'express';
 import NodeCache from 'node-cache';
+import pg from 'pg';
+
+
+const { Client } = pg;
+
+const client = new Client();
+
+client.connect();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/api', (req, res) => res.send('Hello Fucking World!'));
 
-
 // Bad Request
 app.use((req, res) => {
-    res.status(400).json({
-        id: 400,
-        status: 400,
-        code: 'BAD_REQUEST',
-        title: 'Bad Request',
-    });
+  res.status(400).json({
+    id: 400,
+    status: 400,
+    code: 'BAD_REQUEST',
+    title: 'Bad Request',
+  });
 });
 
 // Express goes live
 app.listen(3000, () => {
-    console.log(`Server running on port 3000`);
+  console.log(`Server running on port 3000`);
 });
 
 // Server cache init

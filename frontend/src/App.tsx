d86490c7da7ccec6b0d3f140c9f2c95aebee119e
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2025-02-15 00:48:23 +0100

    feat: dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
 import AppRouter from '@/routing/app-router/AppRouter';
-
+import { Container } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const App = () => {
-  return <AppRouter />;
+  const theme = createTheme({
+    colorSchemes: {
+      dark: true,
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme} defaultMode='light'>
+      <CssBaseline />
+      <Container maxWidth={false} disableGutters>
+        <AppRouter />
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,14 +9,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 
-import CssBaseline from '@mui/material/CssBaseline';
-
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <CssBaseline />
       <App />
     </BrowserRouter>
   </StrictMode>

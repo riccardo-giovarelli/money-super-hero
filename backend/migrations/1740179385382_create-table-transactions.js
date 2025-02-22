20861892a9f614ac17a34aa525d32d20ada2ef53
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2025-02-22 01:37:44 +0100

    feat: new transactions migrations

---
+++ backend/migrations/1740179385382_create-table-transactions.js
@@ -0,0 +1,26 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export function up(pgm) {
+    pgm.createTable('transactions', {
+        id: 'id',
+        user_id: { type: 'integer', notNull: true, references: 'users' },
+        timestamp: { type: 'timestamp', notNull: true, default: pgm.func('current_timestamp') },
+        amount: { type: 'decimal', notNull: true },
+        direction: { type: 'enum', notNull: true, values: ['IN', 'OUT'] },
+        category: { type: 'integer', references: 'categories' },
+        sub_category: { type: 'integer', references: 'sub_categories' },
+        notes: { type: 'text' },
+    });
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export function down(pgm) {
+    pgm.dropTable('transactions');
+}
---
+++ backend/migrations/1740184128831_create-table-sub-categories.js
@@ -0,0 +1,30 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export function up(pgm) {
+    pgm.createTable('sub_categories', {
+        id: 'id',
+        category_id: { type: 'integer', notNull: true, references: 'categories' },
+        name: { type: 'varchar(100)', notNull: true },
+        notes: { type: 'varchar(255)' },
+        createdAt: {
+            type: 'timestamp',
+            notNull: true,
+            default: pgm.func('current_timestamp'),
+        },
+        updatedAt: {
+            type: 'timestamp',
+            notNull: true,
+            default: pgm.func('current_timestamp'),
+        },
+    });
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export function down(pgm) { }
